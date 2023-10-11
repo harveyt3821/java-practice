6e3c272fdbb998900d5cba2bdab54d279ef015f4
harveyt3821 <120616716+harveyt3821@users.noreply.github.com>
2023-10-12 00:58:26 +0800

    Create EmptyDeckException.java

---
+++ PokerCardExercise/EmptyDeckException.java
@@ -0,0 +1,10 @@
+package java_module.collections.question.PokerCardExercise;
+
+/*
+    Do not modify this class
+ */
+public class EmptyDeckException extends RuntimeException {
+    public EmptyDeckException(String message) {
+        super(message);
+    }
+}
