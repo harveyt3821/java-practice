7a9aaaa19006a6686e4460cfc8078b7be59703ac
harveyt3821 <120616716+harveyt3821@users.noreply.github.com>
2023-10-12 00:55:43 +0800

    Create PatternType.java

---
+++ PokerCardExercise/PatternType.java
@@ -0,0 +1,14 @@
+package java_module.collections.question.PokerCardExercise;
+
+public enum PatternType {
+    ROYAL_FLUSH,
+    STRAIGHT_FLUSH,
+    FOUL_OF_A_KIND,
+    FULL_HOUSE,
+    FLUSH,
+    STRAIGHT,
+    THREE_OF_A_KIND,
+    TWO_PAIR,
+    PAIR,
+    NO_PATTERN
+}
